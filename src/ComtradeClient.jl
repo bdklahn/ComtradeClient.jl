74678615172e390b743ec473ad20676dccd8ba65
Brian D. Klahn <briandk@virginia.edu>
2024-11-10 00:43:54 -0500

    Add function to pull H6 categories and create an id to text lookup feather file.

--- src/ComtradeClient.jl
+++ src/ComtradeClient.jl
@@ -1,8 +1,8 @@
 module ComtradeClient
 
-using URIs, HTTP, JSON3, CodecZlib, Arrow, CSV, DataFrames, CategoricalArrays
+using URIs, HTTP, JSON3, CodecZlib, Arrow, CSV, DataFrames, CategoricalArrays, StructTypes
 
-export pull
+export pull, get_gen_H6
 
 include("constants.jl")
 include("pull.jl")
--- src/pull.jl
+++ src/pull.jl
@@ -1,4 +1,37 @@
-# https://comtradeapi.un.org/bulk/v1/get/{typeCode}/{freqCode}/{clCode}[?reporterCode][&period][&publishedDateFrom][&publishedDateTo]
+# to only extract the id and text of the results in H6.json
+struct H6Result
+    id::String
+    text::String
+end
+
+struct H6
+    results::Array{H6Result, 1}
+end
+
+StructTypes.StructType(::Type{H6Result}) = StructTypes.Struct()
+StructTypes.StructType(::Type{H6}) = StructTypes.Struct()
+
+categ_compress(v) = categorical(v, compress = true)
+
+"""
+Get the H6 classifications and generate a
+feather id to text lookup table
+"""
+function get_gen_H6(outdir::String="./")
+    jsn_str = String(HTTP.get(H6_json_url).body)
+    jsn = JSON3.read(jsn_str)
+    open(joinpath(outdir, "H6.json"), "w") do io
+        JSON3.pretty(io, jsn)
+    end
+    h6_results = JSON3.read(jsn_str, H6).results
+    df = DataFrame(id = [r.id for r in h6_results], text = [r.text for r in h6_results])
+    transform!(df, [:id, :text] .=> categ_compress, renamecols=false)
+    open(joinpath(outdir, "H6_id_text.feather"), "w") do io
+        Arrow.write(io, df)
+    end
+    jsn
+end
+
 """
 Generate standard URI's for Comtrade endpoints.
 """
@@ -35,10 +68,9 @@ function pull(domain::String=domain;
     uri = URI(;scheme="https", host=domain, path, query)
     @info uri
 
-    metadata_resp = HTTP.get(uri; headers)
-    jsn = JSON3.read(String(metadata_resp.body))
+    metadata_json = JSON3.read(String(HTTP.get(uri; headers).body))
 
-    for d in jsn.data
+    for d in metadata_json.data[1:1]
         hash = "$(d[:rowKey])"
 
         metadatafile = j(outdir, "$(hash)_metadata.json")
@@ -50,9 +82,9 @@ function pull(domain::String=domain;
         open(outfile, "w") do io
             r = HTTP.get(d[:fileUrl], headers, response_stream=IOBuffer())
             df = CSV.read(transcode(GzipDecompressor, take!(r.body)), DataFrame; downcast=true)
-            transform!(df, categoricals .=> categorical, renamecols=false)
+            transform!(df, categoricals .=> categ_compress, renamecols=false)
             Arrow.write(io, df)
         end
     end
-    jsn
+    metadata_json
 end
